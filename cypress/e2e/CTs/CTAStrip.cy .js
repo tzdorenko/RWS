ac944dc77b67db3eff8c5eb2a3d54b563d42ece9
Tetiana Zdorenko <tanyazdorenkoo@gmail.com>
2026-02-18 14:57:11 +0200

    Add new visual studio tests

---
+++ cypress/e2e/CTs/CTAStrip.cy .js
@@ -0,0 +1,19 @@
+describe('Banner component test on page', () => {
+    beforeEach(() => {
+        // Відкриваємо сторінку
+        cy.visit('cypress/e2e/CTs/BrightcoveContentBlockPlayer.cy.js');
+
+        // Закриваємо кукі банер, якщо він є
+        cy.acceptCookies();
+    });
+
+    it('Checks the banner component and takes a screenshot', () => {
+        // Перевіряємо, що компонент існує і видимий
+        cy.get('section[id="274135"]', { timeout: 5000 }).should('exist').and('be.visible');
+
+        // Робимо скріншот компонента
+        cy.get('section[id="274135"]').screenshot(
+            'cp cp--white banner_with_box variant--banner-with-box first',
+        );
+    });
+});
--- cypress/e2e/CTs/CaseStudyDigital.cy .js
+++ cypress/e2e/CTs/CaseStudyDigital.cy .js
@@ -1,7 +1,7 @@
 describe('Banner component test on page', () => {
     beforeEach(() => {
         // Відкриваємо сторінку
-        cy.visit('https://www.rws.com/test/git/redesign-2025/banner-with-box-small/');
+        cy.visit('https://www.rws.com/test/git/case-study-digital/');
 
         // Закриваємо кукі банер, якщо він є
         cy.acceptCookies();
@@ -9,9 +9,9 @@ describe('Banner component test on page', () => {
 
     it('Checks the banner component and takes a screenshot', () => {
         // Перевіряємо, що компонент існує і видимий
-        cy.get('section[id="275224"]', { timeout: 5000 }).should('exist').and('be.visible');
+        cy.get('section[id="284638"]', { timeout: 5000 }).should('exist').and('be.visible');
 
         // Робимо скріншот компонента
-        cy.get('section[id="275224"]').screenshot('banner-with-box');
+        cy.get('section[id="284638"]').screenshot('cp cp--default case_study_digital');
     });
 });

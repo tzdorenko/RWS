b5e1c58e989daa05ef9945eff277c7cb9fb4ef00
Tetiana Zdorenko <tanyazdorenkoo@gmail.com>
2026-04-14 20:19:02 +0300

    Updated some tests

--- cypress/e2e/CTs/ContBlockBannerImgPodcast.cy.js
+++ cypress/e2e/CTs/ContBlockBannerImgPodcast.cy.js
@@ -1,17 +1,16 @@
-describe('Banner component test on page', () => {
-    beforeEach(() => {
-        // Відкриваємо сторінку
-        cy.visit('https://www.rws.com/test/git/content-block-banner-autoplay-one-case/');
+Cypress.on('uncaught:exception', () => {
+    return false;
+});
 
-        // Закриваємо кукі банер, якщо він є
-        cy.acceptCookies();
+describe('Banner component test - section 233168', () => {
+    beforeEach(() => {
+        cy.visit('https://www.rws.com/test/git/content-block-banner-autoplay-one-case/');
+        cy.acceptCookies({ timeout: 10000 });
     });
 
-    it('Checks the banner component and takes a screenshot', () => {
-        // Перевіряємо, що компонент існує і видимий
-        cy.get('section[id="233168"]', { timeout: 5000 }).should('exist').and('be.visible');
+    it('Checks section 233168 and takes screenshot', () => {
+        cy.get('section[id="233168"]', { timeout: 20000 }).should('exist').and('be.visible');
 
-        // Робимо скріншот компонента
-        cy.get('section[id="233168"]').screenshot('cp cp--original cp--default  ');
+        cy.get('section[id="233168"]').scrollIntoView().screenshot('section-233168');
     });
 });
--- cypress/e2e/CTs/ContBlockCTAImgLeft.cy.js
+++ cypress/e2e/CTs/ContBlockCTAImgLeft.cy.js
@@ -1,18 +1,23 @@
-describe('Banner component test on page', () => {
+Cypress.on('uncaught:exception', () => false);
+
+describe('Banner component test - section 275496', () => {
     beforeEach(() => {
+        cy.setCookie('OptanonConsent', 'isIABGlobal=false&datestamp=consented');
+        cy.setCookie('OptanonAlertBoxClosed', 'true');
+
         cy.visit('https://www.rws.com/test/git/content-block-cta-img-left-rght/');
-        cy.acceptCookies();
     });
 
-    it('Checks the banner component and takes a screenshot', () => {
+    it('Checks section 275496 and takes screenshot', () => {
         cy.get('section#275496', { timeout: 10000 })
-            .scrollIntoView()
+            .scrollIntoView({ block: 'center' })
             .should('exist')
             .and('be.visible');
 
-        cy.wait(500);
+        cy.wait(200); // даємо layout стабілізуватись
 
-        cy.get('section#275496').screenshot('content-block-275496');
+        cy.get('section#275496').screenshot('content-block-275496', {
+            capture: 'viewport',
+        });
     });
 });
-``;
